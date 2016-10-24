dbf9c64082584ceeaf661a7d0668e3f2080c9ba3
Victor Szoltysek <victorszoltysek@Victors-MacBook-Pro.local>
2016-10-23 20:52:42 -0400

    Adding npm install nightmare.

--- drone-io-start-script.sh
+++ drone-io-start-script.sh
@@ -3,5 +3,7 @@
 # install newer version of node as drone-io doesn't have latest
 nvm install v6.2.2 
 
+npm install nightmare
+
 # run xvfb-run which is required for running headless NightMareJs
 sudo xvfb-run -e /dev/stdout -a /home/ubuntu/nvm/v6.2.2/bin/node example.js
