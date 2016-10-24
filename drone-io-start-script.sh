c6f9f1d5b605043ea465bcbe5a8cf384b1f2df14
Victor Szoltysek <victorszoltysek@Victors-MacBook-Pro.local>
2016-10-23 20:49:04 -0400

    Adding drone.io start script.

---
+++ drone-io-start-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install newer version of node as drone-io doesn't have latest
+nvm install v6.2.2 
+
+# run xvfb-run which is required for running headless NightMareJs
+sudo xvfb-run -e /dev/stdout -a /home/ubuntu/nvm/v6.2.2/bin/node example.js
